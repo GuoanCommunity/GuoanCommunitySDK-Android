3423729821779934176a64a27b8c079381be1a79
冯悃 <fengkun19890@gmail.com>
2018-05-10 16:56:26 +0800

    sdk demo更新

--- app/src/main/java/com/guoan/sdkdemo/PayActivity.kt
+++ app/src/main/java/com/guoan/sdkdemo/PayActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import com.guoan.community.sdk.CommunityConstants
 import kotlinx.android.synthetic.main.activity_pay.*
 import org.jetbrains.anko.onClick
-import java.math.BigDecimal
 
 /**
  * @author andylove
@@ -20,7 +19,7 @@ class PayActivity : AppCompatActivity() {
         //支付
         pay_success?.onClick {
             orderid?.text = intent?.getStringExtra("orderId")
-            paymoney?.text = (intent?.getSerializableExtra("payMoney") as BigDecimal)?.toString()
+            paymoney?.text = intent?.getStringExtra("payMoney")
             //支付结束后回调
             MyApplication.sJsInterface?.callPayed(intent?.getStringExtra("responseId"), CommunityConstants.SDK_SUCCESS)
             finish()
